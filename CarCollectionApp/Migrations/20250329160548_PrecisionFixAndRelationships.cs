41fe6226d1cbafeb94f9702a324c62f35131cb88
MikeGisginis <mikegisginis@gmail.com>
2025-04-27 10:56:46 +0300

    Migrations/Data updated

---
+++ CarCollectionApp/Migrations/20250329160548_PrecisionFixAndRelationships.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCollectionApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrecisionFixAndRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Categories_CategoryId",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoinDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Reviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DoorCount",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Seats",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DealerId",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FuelType",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Transmission",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Brands",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_DealerId",
+                table: "Cars",
+                column: "DealerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Categories_CategoryId",
+                table: "Cars",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Dealers_DealerId",
+                table: "Cars",
+                column: "DealerId",
+                principalTable: "Dealers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Categories_CategoryId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Dealers_DealerId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_DealerId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "JoinDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "DoorCount",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Seats",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "DealerId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "FuelType",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Transmission",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Brands");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Categories_CategoryId",
+                table: "Cars",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
